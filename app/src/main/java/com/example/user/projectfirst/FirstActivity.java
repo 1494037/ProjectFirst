0fda9c48fd74c6a2e19836df65bb8f5ffea044fc
HavenRoyal <31908606+HavenRoyal@users.noreply.github.com>
2017-11-17 17:03:38 +0900

    test2-2

--- app/src/main/java/com/example/user/projectfirst/FirstActivity.java
+++ app/src/main/java/com/example/user/projectfirst/FirstActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -76,5 +77,15 @@ public class FirstActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
 
+            case R.id.add_store:
+                startActivity(new Intent(this, addActivity.class));
+                return true;
+            default:
+
+        return super.onOptionsItemSelected(item);
+    }
 }
