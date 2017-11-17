2716252865d3d24007bf9008e266e6b40e90cdf9
HavenRoyal <31908606+HavenRoyal@users.noreply.github.com>
2017-11-17 16:54:05 +0900

    test2-1
    중간올려요

--- app/src/main/java/com/example/user/projectfirst/FirstActivity.java
+++ app/src/main/java/com/example/user/projectfirst/FirstActivity.java
@@ -6,6 +6,8 @@ import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -66,4 +68,13 @@ public class FirstActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
