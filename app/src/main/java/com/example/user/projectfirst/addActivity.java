0d67709aff587464bf70966df0c80de8fdd873f1
HavenRoyal <31908606+HavenRoyal@users.noreply.github.com>
2017-11-17 17:19:20 +0900

    test2-3

--- app/src/main/java/com/example/user/projectfirst/addActivity.java
+++ app/src/main/java/com/example/user/projectfirst/addActivity.java
@@ -1,25 +1,112 @@
 package com.example.user.projectfirst;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.File;
 
 public class addActivity extends AppCompatActivity {
-
+    private static String TAG="MultimediaTest";
+    private ListView mListView;
+    private int mSelectePoistion;
+    private MediaItemAdapter mAdapter;
+    private MediaPlayer mMediaPlayer;
+    private MediaRecorder mMediaRecorder;
+    private String mVideoFileName = null;
+    private File mPhotoFile =null;
+    private String mPhotoFileName = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            Drawable drawable = getDrawable(R.drawable.ic_keyboard_arrow_left_black_24dp);
-            if (drawable != null) {
-                drawable.setTint(Color.WHITE);
-                actionBar.setHomeAsUpIndicator(drawable);
+
+        if (savedInstanceState != null) // 액티비티가 재시작되는 경우, 기존에 저장한 상태 복구
+            mPhotoFileName = savedInstanceState.getString("mPhotoFileName");
+
+
+
+        Button imageCaptureBtn = (Button) findViewById(R.id.imageCaptureBtn);
+
+        checkDangerousPermissions();
+
+        imageCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+
+    }
+    static final int REQUEST_IMAGE_PICK = 0;
+
+    private void dispatchPickPictureIntent() {
+        Intent pickPictureIntent = new Intent(Intent.ACTION_PICK);
+        pickPictureIntent.setType("image/*");
+
+        if (pickPictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(pickPictureIntent,REQUEST_IMAGE_PICK);
+        }
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
+            mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
+            mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+            if (mPhotoFile !=null) {
+                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
+                Uri imageUri = FileProvider.getUriForFile(this, "com.example.kwanwoo.multimediatest", mPhotoFile);
+
+                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } else
+                Toast.makeText(getApplicationContext(), "file null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    final int  REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA=1;
+
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
             }
         }
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA);
+
+        }
+
     }
 }
